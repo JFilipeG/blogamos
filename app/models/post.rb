bee342220324f5c229705903108e01497983c5d3
André Macedo <andrefrmacedo@gmail.com>
2017-07-12 16:37:02 +0100

    Fix post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
-  validates :text, presence: true
+  validates :title, :text, presence: true
 
   def author
     user.email
